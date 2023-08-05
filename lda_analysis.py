3d47e7b24f001cf91261d90ba46c61c6ee072073
Ben S <50429213+beenste-dataman@users.noreply.github.com>
2023-08-05 00:55:47 -0700

    Update lda_analysis.py

--- lda_analysis.py
+++ lda_analysis.py
@@ -2,65 +2,88 @@ import os
 import re
 import nltk
 import pandas as pd
+from tqdm import tqdm  # For the progress bar
 from nltk.corpus import stopwords
 from gensim import corpora, models
 import pyLDAvis.gensim_models
 
-# Load NLTK's English stop-words list
-nltk.download('punkt')
-nltk.download("stopwords")
-stop_words = stopwords.words('english')
+def get_directory_path():
+    print("📂 Welcome to the LDA Analysis Tool!")
+    print("This tool analyzes file names in a directory and categorizes them into topics.")
+    print("It then saves a CSV file containing the file's path, name, dominant topic, and the top words for that topic.")
+    print("Lastly, a visualization (lda.html) of the topics is generated.")
+    print("\nLet's begin...\n")
 
-# Specify the path to your directory
-dir_path = "/content/sample_data/"
-file_names = [f for f in os.listdir(dir_path) if os.path.isfile(os.path.join(dir_path, f))]
+    while True:
+        dir_path = input("🔍 Please enter the full path to your directory: ").strip()
+        if os.path.exists(dir_path) and os.path.isdir(dir_path):
+            return dir_path
+        else:
+            print("❌ Directory not found. Please enter a valid directory path.")
 
-# Remove file extensions, punctuation, and special characters from file names
-file_names_cleaned = [re.sub(r'\.[^.]*$', '', name) for name in file_names]  # Remove file extensions
-file_names_cleaned = [re.sub(r'[_-]+', ' ', name) for name in file_names_cleaned]  # Replace underscores and dashes with spaces
-file_names_cleaned = [re.sub(r'\W+', ' ', name) for name in file_names_cleaned]  # Remove non-word characters
+def main():
+    dir_path = get_directory_path()
 
-# Tokenize the cleaned file names and remove stop words
-file_names_words = [nltk.word_tokenize(name) for name in file_names_cleaned]
-file_names_words = [[word for word in name if word not in stop_words] for name in file_names_words]
+    print("\n⏳ Setting up necessary libraries and data...")
+    nltk.download('punkt', quiet=True)
+    nltk.download("stopwords", quiet=True)
+    stop_words = stopwords.words('english')
 
-# Create a dictionary representation of the documents
-dictionary = corpora.Dictionary(file_names_words)
+    print("⏳ Reading and processing file names...")
+    file_names = [f for f in os.listdir(dir_path) if os.path.isfile(os.path.join(dir_path, f))]
 
-# Convert the list of documents (corpus) into Document-Term Matrix using dictionary prepared above
-doc_term_matrix = [dictionary.doc2bow(name) for name in file_names_words]
+    # Remove file extensions, punctuation, and special characters from file names
+    file_names_cleaned = [re.sub(r'\.[^.]*$', '', name) for name in file_names]  # Remove file extensions
+    file_names_cleaned = [re.sub(r'[_-]+', ' ', name) for name in file_names_cleaned]  # Replace underscores and dashes with spaces
+    file_names_cleaned = [re.sub(r'\W+', ' ', name) for name in file_names_cleaned]  # Remove non-word characters
 
-# Define the number of topics you want
-num_topics = 5
+    # Tokenize the cleaned file names and remove stop words
+    file_names_words = [nltk.word_tokenize(name) for name in file_names_cleaned]
+    file_names_words = [[word for word in name if word not in stop_words] for name in file_names_words]
 
-# Create the object for LDA model using gensim library
-Lda = models.LdaMulticore
+    # Create a dictionary representation of the documents
+    dictionary = corpora.Dictionary(file_names_words)
 
-# Run and train LDA model on the document term matrix
-ldamodel = Lda(doc_term_matrix, num_topics=num_topics, id2word=dictionary, passes=50)
+    # Convert the list of documents (corpus) into Document-Term Matrix using dictionary prepared above
+    doc_term_matrix = [dictionary.doc2bow(name) for name in file_names_words]
 
-# Get the dominant topic for each document
-dominant_topics = [max(ldamodel[doc], key=lambda x: x[1])[0] for doc in doc_term_matrix]
+    # Define the number of topics you want
+    num_topics = 5
 
-# Create a dictionary of topics and their top words
-topic_words = {i: [word_prob[0] for word_prob in ldamodel.show_topic(i, topn=10)] for i in range(num_topics)}
+    # Create the object for LDA model using gensim library
+    Lda = models.LdaMulticore
 
-# Create a DataFrame with the file paths, file names, their dominant topic, and the top words for the dominant topic
-df = pd.DataFrame({
-    'file_path': dir_path,
-    'file_name': file_names,
-    'dominant_topic': dominant_topics,
-    'top_words': [topic_words[topic] for topic in dominant_topics]
-})
+    print("\n⏳ Running LDA analysis... (this may take some time)")
+    for _ in tqdm(range(50), desc="Training LDA model"):  # Wrapping your passes with a progress bar
+        ldamodel = Lda(doc_term_matrix, num_topics=num_topics, id2word=dictionary, passes=1)
 
-# Save the DataFrame to a CSV file
-df.to_csv('file_topics.csv', index=False)
+    # Get the dominant topic for each document
+    dominant_topics = [max(ldamodel[doc], key=lambda x: x[1])[0] for doc in doc_term_matrix]
 
-# Visualization of topics
-vis = pyLDAvis.gensim_models.prepare(ldamodel, doc_term_matrix, dictionary, n_jobs=1)
-pyLDAvis.save_html(vis, 'lda.html')
+    # Create a dictionary of topics and their top words
+    topic_words = {i: [word_prob[0] for word_prob in ldamodel.show_topic(i, topn=10)] for i in range(num_topics)}
 
+    # Create a DataFrame with the file paths, file names, their dominant topic, and the top words for the dominant topic
+    df = pd.DataFrame({
+        'file_path': dir_path,
+        'file_name': file_names,
+        'dominant_topic': dominant_topics,
+        'top_words': [topic_words[topic] for topic in dominant_topics]
+    })
 
-# print words for each topic
-for idx, topic in ldamodel.print_topics(-1):
-    print('Topic: {} \nWords: {}'.format(idx, topic))
+    # Save the DataFrame to a CSV file
+    df.to_csv('file_topics.csv', index=False)
+
+    # Visualization of topics
+    vis = pyLDAvis.gensim_models.prepare(ldamodel, doc_term_matrix, dictionary, n_jobs=1)
+    pyLDAvis.save_html(vis, 'lda.html')
+
+    # print words for each topic 
+    for idx, topic in ldamodel.print_topics(-1):
+        print('Topic: {} \nWords: {}'.format(idx, topic))
+
+    print("\n🎉 Analysis complete! Check the generated lda.html for a visualization of the topics.")
+    print("📄 A CSV file (file_topics.csv) has also been created with the analysis results.")
+
+if __name__ == "__main__":
+    main()
