4335746cc62ad43ef317f717573a9bd14cdef7f3
Ben S <50429213+beenste-dataman@users.noreply.github.com>
2023-08-05 01:47:14 -0700

    Update topic_tapestry.py

--- topic_tapestry.py
+++ topic_tapestry.py
@@ -1,6 +1,8 @@
 import os
 import subprocess
 import time
+from sorting_files import sort_files
+
 
 def check_requirements():
     try:
@@ -42,6 +44,7 @@ def print_ascii():
     """)
     time.sleep(1)  # Let the user see the ASCII art for a bit
 
+
 def main():
     check_requirements()
 
@@ -54,30 +57,34 @@ def main():
         print('-' * 55)
         print('-' * 55)
         print("\nChoose an option:")
-        print("1: Run LDA Analysis")
-        print("2: Query LDA Output Database")
-        print("3: Open Prior LDA Visualization")
-        print("4: Exit")
+        print("1: Sort Files")  # New option added as the first choice
+        print("2: Run LDA Analysis")
+        print("3: Query LDA Output Database")
+        print("4: Open Prior LDA Visualization")
+        print("5: Exit")
         
         choice = input("Enter your choice: ")
         
         # Getting the directory in which this script resides
         current_directory = os.path.dirname(os.path.abspath(__file__))
         
-        if choice == "1":
+        if choice == "1":  # New option added
+            sort_files()
+        elif choice == "2":
             lda_script_path = os.path.join(current_directory, "lda_analysis.py")
             os.system(f'python {lda_script_path}')
-        elif choice == "2":
+        elif choice == "3":
             csv_script_path = os.path.join(current_directory, "csv_query.py")
             os.system(f'python {csv_script_path}')
-        elif choice == "3":
+        elif choice == "4":
             # Building path relative to current script's location for the lda.html file
             lda_html_path = os.path.join(current_directory, "lda.html")
             os.system(f'xdg-open "{lda_html_path}"')
-        elif choice == "4":
+        elif choice == "5":
             break
         else:
             print("Invalid choice. Please try again.")
+
 
 if __name__ == "__main__":
     main()
